5d841434f09408e4fad88aa9d657b8eb8ce1c849
Jace Whitten <jace.whitten@gmail.com>
2020-04-27 04:16:29 -0700

    Remove left over emotion import

--- src/index.js
+++ src/index.js
@@ -3,27 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { Global, css } from '@emotion/core'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Global
-      styles={css`
-        body {
-          font-family: 'Comic Neue', cursive;
-        }
-
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          font-weight: normal;
-          font-family: 'Poppins', sans-serif;
-        }
-      `}
-    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
